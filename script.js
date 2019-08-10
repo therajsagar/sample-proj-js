6d5115f099b3dfcfe156f45979a761a11cf0085d
Raj Sagar <therajsagar@gmail.com>
2019-08-10 18:12:05 +0530

    in-progress

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const addtask = e => {
   const { name } = e.target;
   const inputField = document.querySelector(`div.${name} input`);
-  if (inputField.value) {
+  const value = inputField.value.trim();
+  if (value) {
     //const target = document.getElementById(`${name}-fields`);
     const list = document.querySelectorAll(`div#${name}-fields div`);
     for (i of list) {
       if (!i.innerHTML) {
-        i.innerHTML = inputField.value;
-        inputField.value = '';
+        i.innerHTML = value;
         break;
       }
     }
+    inputField.value = '';
   }
 };
