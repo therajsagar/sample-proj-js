2fe3d12dee0981c305bc74802ecf4e8a288a23b4
Raj Sagar <therajsagar@gmail.com>
2019-08-23 12:31:35 +0530

    updated

--- Comment Widget/script.js
+++ Comment Widget/script.js
@@ -8,16 +8,43 @@ function reIndexing(parentNode) {
   console.log(tasks);
 }
 
+function appendComment(parentnode, value) {
+  const newNode = document.createElement('div');
+  const commentText = document.createElement('span');
+  commentText.innerHTML = value;
+  const replyButton = document.createElement('button');
+  replyButton.innerHTML = 'Reply';
+  replyButton.addEventListener('click', replyOnComment);
+  newNode.appendChild(commentText);
+  newNode.appendChild(replyButton);
+  parentnode.appendChild(newNode);
+  reIndexing(parentnode);
+}
+
 function addComment(ev) {
   ev.preventDefault();
   const commentField = document.querySelector('textarea[name="main-comment"]');
   const value = commentField.value.trim();
   if (value) {
     const commentSection = document.getElementById('mt');
-    const newNode = document.createElement('div');
-    newNode.innerHTML = value;
-    commentSection.appendChild(newNode);
+    appendComment(commentSection, value);
     reIndexing(commentSection);
   }
   commentField.value = '';
+}
+
+function replyOnComment(e) {
+  const ip = document.createElement('textarea');
+  const bt = document.createElement('button');
+  bt.innerHTML = 'reply';
+  const prd = document.createElement('div');
+  prd.appendChild(ip);
+  prd.appendChild(bt);
+  const parentnode = e.target.parentNode;
+  parentnode.appendChild(prd);
+  bt.addEventListener('click', () => {
+    console.log(ip.value);
+    appendComment(parentnode, ip.value);
+    prd.remove();
+  });
 }
