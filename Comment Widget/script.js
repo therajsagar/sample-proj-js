67b0361408b61144bbf652517f66020de9a21ee7
Raj Sagar <therajsagar@gmail.com>
2019-08-26 03:48:25 +0530

    DONE - comment widget

--- Comment Widget/script.js
+++ Comment Widget/script.js
@@ -5,20 +5,47 @@ function reIndexing(parentNode) {
     list[i].setAttribute('id', `${parentNode.id}-${i}`);
     tasks.push(list[i].textContent);
   }
-  console.log(tasks);
 }
 
 function appendComment(parentnode, value) {
   const newNode = document.createElement('div');
   const commentText = document.createElement('span');
   commentText.innerHTML = value;
+
   const replyButton = document.createElement('button');
+  replyButton.setAttribute('class', 'reply');
   replyButton.innerHTML = 'Reply';
   replyButton.addEventListener('click', replyOnComment);
-  newNode.appendChild(commentText);
-  newNode.appendChild(replyButton);
+
+  const deleteButton = document.createElement('button');
+  deleteButton.setAttribute('class', 'delete');
+  deleteButton.innerHTML = 'Delete';
+  deleteButton.addEventListener('click', deleteAComment);
+
+  const editButton = document.createElement('button');
+  editButton.setAttribute('class', 'edit');
+  editButton.innerHTML = 'Edit';
+  editButton.addEventListener('click', editTheComment);
+
+  const likeButton = document.createElement('button');
+  likeButton.setAttribute('class', 'like');
+  likeButton.innerHTML = 'like';
+  likeButton.addEventListener('click', likeTheComment);
+
+  const likeCount = document.createElement('span');
+  likeCount.innerHTML = '0';
+  likeCount.setAttribute('class', 'likeCount');
+
+  const sectionNode = document.createElement('section');
+  sectionNode.appendChild(commentText);
+  sectionNode.appendChild(likeCount);
+  sectionNode.appendChild(likeButton);
+  sectionNode.appendChild(replyButton);
+  sectionNode.appendChild(editButton);
+  sectionNode.appendChild(deleteButton);
+
+  newNode.appendChild(sectionNode);
   parentnode.appendChild(newNode);
-  reIndexing(parentnode);
 }
 
 function addComment(ev) {
@@ -33,18 +60,50 @@ function addComment(ev) {
   commentField.value = '';
 }
 
-function replyOnComment(e) {
-  const ip = document.createElement('textarea');
-  const bt = document.createElement('button');
-  bt.innerHTML = 'reply';
-  const prd = document.createElement('div');
-  prd.appendChild(ip);
-  prd.appendChild(bt);
-  const parentnode = e.target.parentNode;
-  parentnode.appendChild(prd);
-  bt.addEventListener('click', () => {
-    console.log(ip.value);
-    appendComment(parentnode, ip.value);
-    prd.remove();
+function commentOperations(parentNode, type) {
+  disableEnableAllButtons(true);
+  const input = document.createElement('textarea');
+  const tempButton = document.createElement('button');
+  tempButton.innerHTML = 'Done';
+  tempButton.setAttribute('class', 'reply');
+  const replyWidget = document.createElement('div');
+  replyWidget.appendChild(input);
+  replyWidget.appendChild(tempButton);
+  parentNode.appendChild(replyWidget);
+  tempButton.addEventListener('click', () => {
+    const value = input.value.trim();
+    if (value) {
+      if (type === 'reply') {
+        appendComment(parentNode, input.value);
+      } else {
+        parentNode.children[0].innerHTML = input.value;
+      }
+      replyWidget.remove();
+      disableEnableAllButtons(false);
+    }
   });
+}
+
+function replyOnComment(e) {
+  const parentnode = e.target.parentNode;
+  commentOperations(parentnode, 'reply');
+}
+
+function deleteAComment(e) {
+  e.target.parentNode.parentNode.remove();
+}
+
+function editTheComment(e) {
+  const parentnode = e.target.parentNode;
+  commentOperations(parentnode, 'edit');
+}
+
+function disableEnableAllButtons(disable) {
+  const buttonArray = document.querySelectorAll('button');
+  buttonArray.forEach(bt => (bt.disabled = disable));
+}
+
+function likeTheComment(e) {
+  const likes = e.target.previousSibling;
+  likes.innerHTML = Number(likes.innerHTML) + 1;
 }
